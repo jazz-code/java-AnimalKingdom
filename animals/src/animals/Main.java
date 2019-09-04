8b4a21c0b3f98f0a9bd011883d0164d3caa83e05
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-04 18:34:08 -0400

    Made arraylist from AbstractAnimal and sorted in descending order by year

--- animals/src/animals/Main.java
+++ animals/src/animals/Main.java
@@ -1,18 +1,63 @@
 package animals;
 
+import java.util.*;
+
 public class Main 
 {
     public static void main(String[] args)
     {
-        System.out.println("***Mammals***");
+        //--Mammals---*
         Mammals panda = new Mammals("Panda", 1869);
         Mammals zebra = new Mammals("Zebra", 1778);
         Mammals koala = new Mammals("Koala", 1816);
         Mammals sloth = new Mammals("Sloth", 1804);
         Mammals armadillo = new Mammals("Armadillo", 1758);
-        Mammals racoon = new Mammals("Raccoon", 1758);
+        Mammals raccoon = new Mammals("Raccoon", 1758);
         Mammals bigfoot = new Mammals("Bigfoot", 2021);
+        //---Birds---*
+        Birds pigeon = new Birds("Pigeon", 1837);
+		Birds peacock = new Birds("Peacock", 1821);
+		Birds toucan = new Birds("Toucan", 1758);
+		Birds parrot = new Birds("Parrot", 1824);
+		Birds swan = new Birds("Swan", 1758);
+        //---Fish---*
+		Fish salmon = new Fish("Salmon", 1758);
+		Fish catfish = new Fish("CatFish", 1817);
+        Fish perch = new Fish("Perch", 1758);
+        
+        //--Prints name and year discovered from different classes----*
+        System.out.println(panda.getName() + " " + "Year Discovered: " + panda.getYearDiscovered());
+        System.out.println(pigeon.getName() + " " + "Year Discovered: " + pigeon.getYearDiscovered());
+        System.out.println(salmon.getName() + " " + "Year Discovered: " + salmon.getYearDiscovered());
 
-        System.out.println(panda.name + panda.yearDiscovered);
+        //-- New ArrayList combines classes from AbstractAnimal--*
+        ArrayList<AbstractAnimal> myList = new ArrayList<AbstractAnimal>();
+        //Add all to arrayList
+        System.out.println("***ArrayList***");
+		myList.add(panda);
+		myList.add(zebra);
+		myList.add(koala);
+		myList.add(sloth);
+		myList.add(armadillo);
+		myList.add(raccoon);
+		myList.add(bigfoot);
+		myList.add(pigeon);
+		myList.add(peacock);
+		myList.add(toucan);
+		myList.add(parrot);
+		myList.add(swan);
+		myList.add(salmon);
+		myList.add(catfish);
+		myList.add(perch);
+        System.out.println("*****");
+
+        
+        myList.forEach((v) -> System.out.println(v.getName()));
+        
+        //--List all the animals in descending order by year named--*
+        myList.sort((v1, v2) -> v2.getYearDiscovered() - v1.getYearDiscovered());
+        myList.forEach(animal -> System.out.println(animal.getName() + " " + animal.getYearDiscovered()));
+        
+        // List all the animals alphabetically
     }
 }
